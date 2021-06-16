afd1791b724029604560dbbebf71124311bb78d5
yamaton <yamaton@gmail.com>
2021-06-16 00:09:13 -0400

    Add seqtk, fastqc

--- scripts/make-gzip.sh
+++ scripts/make-gzip.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 basedir="$(dirname "$(readlink -f "$0")")"
+
+ls "$basedir"/../json/*.json | parallel python "$basedir"/validate-json.py
 jq -s . "$basedir"/../json/*.json | gzip > "$basedir"/../all.json.gz

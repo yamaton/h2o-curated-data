88b7aaa144411f230df155e145892f29c3dce878
Yamato Matsuoka <yamaton@gmail.com>
2022-02-23 13:59:27 -0500

    Minify packed JSONs

--- scripts/make-gzip.sh
+++ scripts/make-gzip.sh
@@ -11,7 +11,7 @@ basedir="$(dirname "$(readlink -f "$0")")"
 "$basedir"/validate-all.py
 
 ls "$basedir"/../general/json/*.json | parallel python "$basedir"/validate.py
-jq -s . "$basedir"/../general/json/*.json | gzip > "$basedir"/../general.json.gz
+jq -cs . "$basedir"/../general/json/*.json | gzip > "$basedir"/../general.json.gz
 
 ls "$basedir"/../bio/json/*.json | parallel python "$basedir"/validate.py
-jq -s . "$basedir"/../bio/json/*.json | gzip > "$basedir"/../bio.json.gz
+jq -cs . "$basedir"/../bio/json/*.json | gzip > "$basedir"/../bio.json.gz

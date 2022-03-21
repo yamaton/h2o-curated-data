4eb6bfd282775b9c87d843feb5eed786755ad7b8
yamaton <yamaton@gmail.com>
2022-03-21 09:06:57 -0400

    Add procs to experimental

---
+++ experimental/fish/procs.fish
@@ -0,0 +1,25 @@
+# Auto-generated with h2o
+
+complete -c procs -s a -l and -d 'AND logic for multi-keyword'
+complete -c procs -s o -l or -d 'OR logic for multi-keyword'
+complete -c procs -s d -l nand -d 'NAND logic for multi-keyword'
+complete -c procs -s r -l nor -d 'NOR logic for multi-keyword'
+complete -c procs -s l -l list -d 'Show list of kind'
+complete -c procs -l thread -d 'Show thread'
+complete -c procs -s t -l tree -d 'Tree view'
+complete -c procs -s w -l watch -d 'Watch mode with default interval (1s)'
+complete -c procs -s W -l watch-interval -d 'Watch mode with custom interval' -x
+complete -c procs -s i -l insert -d 'Insert column to slot' -x
+complete -c procs -l only -d 'Specified column only' -x
+complete -c procs -l sorta -d 'Sort column by ascending' -x
+complete -c procs -l sortd -d 'Sort column by descending' -x
+complete -c procs -s c -l color -d 'Color mode' -x
+complete -c procs -l theme -d 'Theme mode' -x
+complete -c procs -s p -l pager -d 'Pager mode' -x
+complete -c procs -l interval -d 'Interval to calculate throughput' -x
+complete -c procs -l config -d 'Generate configuration sample file'
+complete -c procs -l completion -d 'Generate shell completion file' -x
+complete -c procs -l completion-out -d 'Generate shell completion file and write to stdout' -x
+complete -c procs -l no-header -d 'Suppress header'
+complete -c procs -s h -l help -d 'Print help information'
+complete -c procs -s V -l version -d 'Print version information'

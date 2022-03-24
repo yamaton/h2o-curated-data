53cad2f639c373e11e8c1ca11ab4f709234f5f35
yamaton <yamaton@gmail.com>
2022-03-24 01:47:43 -0400

    Add validate-json-simple.sh

---
+++ scripts/validate-json-simple.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# Use ajv to validate a JSON with a schema
+#
+# Example:
+#   $ ./validate-json-simple.sh jq
+#
+basedir="$(dirname "$(readlink -f "$0")")"
+schema="$basedir"/../json-schema/command-2022-03-14.schema.json
+name="$1"
+json=$(find "$basedir/.." -type f -name "$name".json)
+
+for f in $json; do
+    ajv -s "$schema" -d "$json"
+done

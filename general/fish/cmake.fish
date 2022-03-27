e974db2531a0ff3e2c52c23b4279f303a3a857be
yamaton <yamaton@gmail.com>
2022-03-26 21:05:50 -0400

    Update cmake

--- general/fish/cmake.fish
+++ general/fish/cmake.fish
@@ -3,7 +3,7 @@
 complete -c cmake -s S -d 'Path to root directory of the CMake project to build.' -r
 complete -c cmake -s B -d 'Path to directory which CMake will use as the root of build directory.' -r
 complete -c cmake -s C -d 'Pre-load a script to populate the cache.' -x
-complete -c cmake -s D -s D -d 'Create or update a CMake CACHE entry.' -x
+complete -c cmake -s D -d 'Create or update a CMake CACHE entry.' -x
 complete -c cmake -s U -d 'Remove matching entries from CMake CACHE.' -x
 complete -c cmake -s G -d 'Specify a build system generator.' -x
 complete -c cmake -s T -d 'Toolset specification for the generator, if supported.' -x
@@ -45,29 +45,25 @@ complete -c cmake -l component -d 'Component-based install.' -x
 complete -c cmake -l prefix -d 'Override the installation prefix, CMAKE_INSTALL_PREFIX.' -x
 complete -c cmake -l strip -d 'Strip before installing.'
 complete -c cmake -s v -l verbose -d 'Enable verbose output.'
-complete -c cmake -l help -o help -o usage -s h -s H -d 'Print usage information and exit.'
-complete -c cmake -l version -o version -d 'Show program name/version banner and exit.'
+complete -c cmake -l zstd -d 'Compress the resulting archive with Zstandard.'
+complete -c cmake -l files-from -d 'Read file names from the given file, one per line.' -r
+complete -c cmake -l format -d 'Specify the format of the archive to be created.' -x
+complete -c cmake -l mtime -d 'Specify modification time recorded in tarball entries.' -x
 complete -c cmake -l help-full -d 'Print all help manuals and exit.' -x
-complete -c cmake -l help-manual -d '[<f>]   Print one help manual and exit.' -x
 complete -c cmake -l help-manual -d 'Print one help manual and exit.' -x
 complete -c cmake -l help-manual-list -d 'List help manuals available and exit.' -x
-complete -c cmake -l help-command -d '[<f>]   Print help for one command and exit.' -x
 complete -c cmake -l help-command -d 'Print help for one command and exit.' -x
 complete -c cmake -l help-command-list -d 'List commands with help available and exit.' -x
 complete -c cmake -l help-commands -d 'Print cmake-commands manual and exit.' -x
-complete -c cmake -l help-module -d '[<f>]   Print help for one module and exit.' -x
 complete -c cmake -l help-module -d 'Print help for one module and exit.' -x
 complete -c cmake -l help-module-list -d 'List modules with help available and exit.' -x
 complete -c cmake -l help-modules -d 'Print cmake-modules manual and exit.' -x
-complete -c cmake -l help-policy -d '[<f>]   Print help for one policy and exit.' -x
 complete -c cmake -l help-policy -d 'Print help for one policy and exit.' -x
 complete -c cmake -l help-policy-list -d 'List policies with help available and exit.' -x
 complete -c cmake -l help-policies -d 'Print cmake-policies manual and exit.' -x
-complete -c cmake -l help-property -d '[<f>]   Print help for one property and exit.' -x
 complete -c cmake -l help-property -d 'Print help for one property and exit.' -x
 complete -c cmake -l help-property-list -d 'List properties with help available and exit.' -x
 complete -c cmake -l help-properties -d 'Print cmake-properties manual and exit.' -x
-complete -c cmake -l help-variable -d '[<f>]   Print help for one variable and exit.' -x
 complete -c cmake -l help-variable -d 'Print help for one variable and exit.' -x
 complete -c cmake -l help-variable-list -d 'List variables with help available and exit.' -x
 complete -c cmake -l help-variables -d 'Print cmake-variables manual and exit.' -x

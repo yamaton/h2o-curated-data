4cc6bab25da1f0015dd82d455eb9ba793937f417
yamaton <yamaton@gmail.com>
2022-05-22 09:59:24 -0400

    Add gzip/gunzip

---
+++ general/fish/gzip.fish
@@ -0,0 +1,20 @@
+# Auto-generated with h2o
+
+complete -c gzip -s c -l stdout -d 'write on standard output, keep original files unchanged'
+complete -c gzip -s d -l decompress -s f -l force -d 'force overwrite of output file and compress links' -x
+complete -c gzip -s h -l help -d 'give this help'
+complete -c gzip -s k -l keep -d 'keep (don\'t delete) input files'
+complete -c gzip -s l -l list -d 'list compressed file contents'
+complete -c gzip -s L -l license -d 'display software license'
+complete -c gzip -s n -l no-name -d 'do not save or restore the original name and timestamp'
+complete -c gzip -s N -l name -d 'save or restore the original name and timestamp'
+complete -c gzip -s q -l quiet -d 'suppress all warnings'
+complete -c gzip -s r -l recursive -d 'operate recursively on directories'
+complete -c gzip -l rsyncable -d 'make rsync-friendly archive'
+complete -c gzip -s S -l suffix -d 'use suffix SUF on compressed files' -x
+complete -c gzip -l synchronous -d 'synchronous output (safer if system crashes, but slower)'
+complete -c gzip -s t -l test -d 'test compressed file integrity'
+complete -c gzip -s v -l verbose -d 'verbose mode'
+complete -c gzip -s V -l version -d 'display version number'
+complete -c gzip -s 1 -l fast -d 'compress faster'
+complete -c gzip -s 9 -l best -d 'compress better'

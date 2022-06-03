f468c5893e3b7d6eedde915a29c3a6e1fbb93880
Yamato Matsuoka <yamaton@gmail.com>
2022-06-03 00:36:48 -0400

    Add ansible and its siblings

---
+++ general/fish/ansible-inventory.fish
@@ -0,0 +1,19 @@
+# Auto-generated with h2o
+
+complete -c ansible-inventory -l ask-vault-password -l ask-vault-pass -d 'ask for vault password'
+complete -c ansible-inventory -l export -d 'When doing an --list, represent in a way that is optimized for export,not as an accurate representation of how Ansible has processed it'
+complete -c ansible-inventory -l output -d 'When doing --list, send the inventory to a file instead of to the screen' -r
+complete -c ansible-inventory -l playbook-dir -d 'Since this tool does not use playbooks, use this as a substitute playbook directory.' -r
+complete -c ansible-inventory -l toml -d 'Use TOML format instead of default JSON, ignored for --graph'
+complete -c ansible-inventory -l vars -d 'Add vars to graph display, ignored unless used with --graph'
+complete -c ansible-inventory -l vault-id -d 'the vault identity to use' -x
+complete -c ansible-inventory -l vault-password-file -l vault-pass-file -d 'vault password file' -r
+complete -c ansible-inventory -l version -d 'show program\'s version number, config file location, configured module search path, module location, executable location and exit'
+complete -c ansible-inventory -s e -l extra-vars -d 'set additional variables as key=value or YAML/JSON, if filename prepend with @' -r
+complete -c ansible-inventory -s h -l help -d 'show this help message and exit'
+complete -c ansible-inventory -s i -l inventory -l inventory-file -d 'specify inventory host path or comma separated host list.' -r
+complete -c ansible-inventory -s v -l verbose -d 'Causes Ansible to print more debug messages.'
+complete -c ansible-inventory -s y -l yaml -d 'Use YAML format instead of default JSON, ignored for --graph'
+complete -c ansible-inventory -l graph -d 'create inventory graph, if supplying pattern it must be a valid group name'
+complete -c ansible-inventory -l host -d 'Output specific host info, works as inventory script' -x
+complete -c ansible-inventory -l list -d 'Output all hosts info, works as inventory script'
---
+++ general/fish/ansible-pull.fish
@@ -0,0 +1,42 @@
+# Auto-generated with h2o
+
+complete -c ansible-pull -l accept-host-key -d 'adds the hostkey for the repo url if not already added'
+complete -c ansible-pull -l ask-vault-password -l ask-vault-pass -d 'ask for vault password'
+complete -c ansible-pull -l become-password-file -l become-pass-file -d 'Become password file' -r
+complete -c ansible-pull -l check -d 'don\'t make any changes; instead, try to predict some of the changes that may occur'
+complete -c ansible-pull -l clean -d 'modified files in the working repository will be discarded'
+complete -c ansible-pull -l connection-password-file -l conn-pass-file -d 'Connection password file' -r
+complete -c ansible-pull -l diff -d 'when changing (small) files and templates, show the differences in those files; works great with --check'
+complete -c ansible-pull -l full -d 'Do a full clone, instead of a shallow one.'
+complete -c ansible-pull -l list-hosts -d 'outputs a list of matching hosts; does not execute anything else'
+complete -c ansible-pull -l purge -d 'purge checkout after playbook run'
+complete -c ansible-pull -l skip-tags -d 'only run plays and tasks whose tags do not match these values' -x
+complete -c ansible-pull -l track-subs -d 'submodules will track the latest changes.'
+complete -c ansible-pull -l vault-id -d 'the vault identity to use' -x
+complete -c ansible-pull -l vault-password-file -l vault-pass-file -d 'vault password file' -r
+complete -c ansible-pull -l verify-commit -d 'verify GPG signature of checked out commit, if it fails abort running the playbook.'
+complete -c ansible-pull -l version -d 'show program\'s version number, config file location, configured module search path, module location, executable location and exit'
+complete -c ansible-pull -s C -l checkout -d 'branch/tag/commit to checkout.' -x
+complete -c ansible-pull -s K -l ask-become-pass -d 'ask for privilege escalation password'
+complete -c ansible-pull -s M -l module-path -d 'prepend colon-separated path(s) to module library (default=~/.ansible/plugins/modules:/usr/share/ansible/plugins/modules)' -r
+complete -c ansible-pull -s U -l url -d 'URL of the playbook repository' -x
+complete -c ansible-pull -s d -l directory -d 'absolute path of repository checkout directory (relative paths are not supported)' -r
+complete -c ansible-pull -s e -l extra-vars -d 'set additional variables as key=value or YAML/JSON, if filename prepend with @' -r
+complete -c ansible-pull -s f -l force -d 'run the playbook even if the repository could not be updated'
+complete -c ansible-pull -s h -l help -d 'show this help message and exit'
+complete -c ansible-pull -s i -l inventory -l inventory-file -d 'specify inventory host path or comma separated host list.' -r
+complete -c ansible-pull -s k -l ask-pass -d 'ask for connection password'
+complete -c ansible-pull -s l -l limit -d 'further limit selected hosts to an additional pattern' -x
+complete -c ansible-pull -s m -l module-name -d 'Repository module name, which ansible will use to check out the repo.' -x
+complete -c ansible-pull -s o -l only-if-changed -d 'only run the playbook if the repository has been updated'
+complete -c ansible-pull -s s -l sleep -d 'sleep for random interval (between 0 and n number of seconds) before starting.' -x
+complete -c ansible-pull -s t -l tags -d 'only run plays and tasks tagged with these values' -x
+complete -c ansible-pull -s v -l verbose -d 'Causes Ansible to print more debug messages.'
+complete -c ansible-pull -l private-key -l key-file -d 'use this file to authenticate the connection' -r
+complete -c ansible-pull -l scp-extra-args -d 'specify extra arguments to pass to scp only (e.g. -l)' -x
+complete -c ansible-pull -l sftp-extra-args -d 'specify extra arguments to pass to sftp only (e.g. -f, -l)' -x
+complete -c ansible-pull -l ssh-common-args -d 'specify common arguments to pass to sftp/scp/ssh (e.g. ProxyCommand)' -x
+complete -c ansible-pull -l ssh-extra-args -d 'specify extra arguments to pass to ssh only (e.g. -R)' -x
+complete -c ansible-pull -s T -l timeout -d 'override the connection timeout in seconds (default=10)' -x
+complete -c ansible-pull -s c -l connection -d 'connection type to use (default=smart)' -x
+complete -c ansible-pull -s u -l user -d 'connect as this user (default=None)' -x

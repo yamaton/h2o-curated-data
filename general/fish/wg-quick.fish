5479dc880d3c18c249631c350990d59139bd92ef
Yamato Matsuoka <yamaton@gmail.com>
2022-07-09 19:17:18 -0400

    Add missed files

---
+++ general/fish/wg-quick.fish
@@ -0,0 +1,6 @@
+# Auto-generated with h2o
+
+complete -k -c wg-quick -n __fish_use_subcommand -x -a strip -d 'output a configuration file with all wg-quick(8)-specific options removed, suitable for use with wg(8)'
+complete -k -c wg-quick -n __fish_use_subcommand -x -a save -d 'saves the configuration of an existing interface without bringing the interface down'
+complete -k -c wg-quick -n __fish_use_subcommand -x -a down -d 'tear down and remove an interface'
+complete -k -c wg-quick -n __fish_use_subcommand -x -a up -d 'add and set up an interface'

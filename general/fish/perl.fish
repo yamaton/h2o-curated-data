c43776c91af66a8f4c26df0758fe904b0738e9d4
Yamato Matsuoka <yamaton@gmail.com>
2022-07-19 22:36:47 -0400

    Add perl

---
+++ general/fish/perl.fish
@@ -0,0 +1,31 @@
+# Auto-generated with h2o
+
+complete -c perl -s 0 -d 'specify record separator (\0, if no argument)' -x
+complete -c perl -s a -d 'autosplit mode with -n or -p (splits $_ into @F)'
+complete -c perl -s C -d 'enables the listed Unicode features' -x
+complete -c perl -s c -d 'check syntax only (runs BEGIN and CHECK blocks)'
+complete -c perl -s d -d 'run program under debugger' -x
+complete -c perl -s D -d 'set debugging flags (argument is a bit mask or alphabets)' -x
+complete -c perl -s e -d 'one line of program (several -e\'s allowed, omit programfile)' -r
+complete -c perl -s E -d 'like -e, but enables all optional features' -x
+complete -c perl -s f -d 'don\'t do $sitelib/sitecustomize.pl at startup'
+complete -c perl -s F -d 'split() pattern for -a switch (//\'s are optional)' -x
+complete -c perl -s i -d 'edit <> files in place (makes backup if extension supplied)' -r
+complete -c perl -o Idirectory -d 'specify @INC/#include directory (several -I\'s allowed)'
+complete -c perl -s l -d 'enable line ending processing, specifies line terminator' -x
+complete -c perl -s m -d 'execute "use module..." before executing program' -x
+complete -c perl -s M -d 'execute "no module..." before executing program' -x
+complete -c perl -s n -d 'assume "while (<>) { ...'
+complete -c perl -s p -d 'assume loop like -n but print line also, like sed'
+complete -c perl -s s -d 'enable rudimentary parsing for switches after programfile'
+complete -c perl -s S -d 'look for programfile using PATH environment variable'
+complete -c perl -s t -d 'enable tainting warnings'
+complete -c perl -s T -d 'enable tainting checks'
+complete -c perl -s u -d 'dump core after parsing program'
+complete -c perl -s U -d 'allow unsafe operations'
+complete -c perl -s v -d 'print version, patchlevel and license'
+complete -c perl -s V -d 'print configuration summary (or a single Config.pm variable)' -x
+complete -c perl -s w -d 'enable many useful warnings'
+complete -c perl -s W -d 'enable all warnings'
+complete -c perl -s x -d 'ignore text before #!perl line (optionally cd to directory)' -r
+complete -c perl -s X -d 'disable all warnings'

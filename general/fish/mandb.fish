db56448bb685e4e8059b962a97efc3b0d31cc5fc
Yamato Matsuoka <yamaton@gmail.com>
2022-07-27 00:35:37 -0400

    Add mandb

---
+++ general/fish/mandb.fish
@@ -0,0 +1,14 @@
+# Auto-generated with h2o
+
+complete -c mandb -s c -l create -d 'create dbs from scratch, rather than updating'
+complete -c mandb -s C -l config-file -d 'use this user configuration file' -r
+complete -c mandb -s d -l debug -d 'emit debugging messages'
+complete -c mandb -s f -l filename -d 'update just the entry for this filename' -r
+complete -c mandb -s p -l no-purge -d 'don\'t purge obsolete entries from the dbs'
+complete -c mandb -s q -l quiet -d 'work quietly, except for \'bogus\' warning'
+complete -c mandb -s s -l no-straycats -d 'don\'t look for or add stray cats to the dbs'
+complete -c mandb -s t -l test -d 'check manual pages for correctness'
+complete -c mandb -s u -l user-db -d 'produce user databases only'
+complete -c mandb -s \? -l help -d 'give this help list'
+complete -c mandb -l usage -d 'give a short usage message'
+complete -c mandb -s V -l version -d 'print program version'

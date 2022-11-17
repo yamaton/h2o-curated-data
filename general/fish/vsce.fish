c9a0c564cb390314cfb5f52e2a133b65af1ddad2
Yamato Matsuoka <yamaton@gmail.com>
2022-11-17 14:53:52 -0500

    Update vsce

--- general/fish/vsce.fish
+++ general/fish/vsce.fish
@@ -1,10 +1,11 @@
 # Auto-generated with h2o
 
-complete -c vsce -n "not __fish_seen_subcommand_from ls package publish unpublish ls-publishers delete-publisher login logout verify-pat show search" -s "V" -l "version" -d "output the version number"
-complete -c vsce -n "not __fish_seen_subcommand_from ls package publish unpublish ls-publishers delete-publisher login logout verify-pat show search" -s "h" -l "help" -d "display help for command"
+complete -c vsce -n "not __fish_seen_subcommand_from ls package publish unpublish ls-publishers delete-publisher login logout verify-pat show search help" -s "V" -l "version" -d "output the version number"
+complete -c vsce -n "not __fish_seen_subcommand_from ls package publish unpublish ls-publishers delete-publisher login logout verify-pat show search help" -s "h" -l "help" -d "display help for command"
 
 
 
+complete -k -c vsce -n __fish_use_subcommand -x -a help -d "display help for command"
 complete -k -c vsce -n __fish_use_subcommand -x -a search -d "search extension gallery"
 complete -k -c vsce -n __fish_use_subcommand -x -a show -d "Show extension metadata"
 complete -k -c vsce -n __fish_use_subcommand -x -a verify-pat -d "Verify if the Personal Access Token has publish rights for the publisher."
@@ -23,13 +24,14 @@ complete -c vsce -n "__fish_seen_subcommand_from ls" -l "yarn" -d "Use yarn inst
 complete -c vsce -n "__fish_seen_subcommand_from ls" -l "no-yarn" -d "Use npm instead of yarn (default inferred from lack of yarn.lock or .yarnrc)"
 complete -c vsce -n "__fish_seen_subcommand_from ls" -l "packagedDependencies" -d "Select packages that should be published only (includes dependencies)" -r
 complete -c vsce -n "__fish_seen_subcommand_from ls" -l "ignoreFile" -d "Indicate alternative .vscodeignore" -r
+complete -c vsce -n "__fish_seen_subcommand_from ls" -l "dependencies" -d "Enable dependency detection via npm or yarn"
 complete -c vsce -n "__fish_seen_subcommand_from ls" -l "no-dependencies" -d "Disable dependency detection via npm or yarn"
 complete -c vsce -n "__fish_seen_subcommand_from ls" -s "h" -l "help" -d "display help for command"
 
 
 
 complete -c vsce -n "__fish_seen_subcommand_from package" -s "o" -l "out" -d "Output .vsix extension file to <path> location (defaults to <name>-<version>.vsix)" -r
-complete -c vsce -n "__fish_seen_subcommand_from package" -s "t" -l "target" -d "Target architecture" -x
+complete -c vsce -n "__fish_seen_subcommand_from package" -s "t" -l "target" -d "Target architecture." -x
 complete -c vsce -n "__fish_seen_subcommand_from package" -s "m" -l "message" -d "Commit message used when calling `npm version`." -x
 complete -c vsce -n "__fish_seen_subcommand_from package" -l "no-git-tag-version" -d "Do not create a version commit and tag when calling `npm version`."
 complete -c vsce -n "__fish_seen_subcommand_from package" -l "no-update-package-json" -d "Do not update `package.json`."
@@ -43,14 +45,17 @@ complete -c vsce -n "__fish_seen_subcommand_from package" -l "no-yarn" -d "Use n
 complete -c vsce -n "__fish_seen_subcommand_from package" -l "ignoreFile" -d "Indicate alternative .vscodeignore" -r
 complete -c vsce -n "__fish_seen_subcommand_from package" -l "no-gitHubIssueLinking" -d "Disable automatic expansion of GitHub-style issue syntax into links"
 complete -c vsce -n "__fish_seen_subcommand_from package" -l "no-gitLabIssueLinking" -d "Disable automatic expansion of GitLab-style issue syntax into links"
+complete -c vsce -n "__fish_seen_subcommand_from package" -l "dependencies" -d "Enable dependency detection via npm or yarn"
 complete -c vsce -n "__fish_seen_subcommand_from package" -l "no-dependencies" -d "Disable dependency detection via npm or yarn"
 complete -c vsce -n "__fish_seen_subcommand_from package" -l "pre-release" -d "Mark this package as a pre-release"
+complete -c vsce -n "__fish_seen_subcommand_from package" -l "allow-star-activation" -d "Allow using * in activation events"
+complete -c vsce -n "__fish_seen_subcommand_from package" -l "allow-missing-repository" -d "Allow missing a repository URL in package.json"
 complete -c vsce -n "__fish_seen_subcommand_from package" -s "h" -l "help" -d "display help for command"
 
 
 
 complete -c vsce -n "__fish_seen_subcommand_from publish" -s "p" -l "pat" -d "Personal Access Token (defaults to VSCE_PAT environment variable)" -x
-complete -c vsce -n "__fish_seen_subcommand_from publish" -s "t" -l "target" -d "Target architectures" -x
+complete -c vsce -n "__fish_seen_subcommand_from publish" -s "t" -l "target" -d "Target architectures." -x
 complete -c vsce -n "__fish_seen_subcommand_from publish" -s "m" -l "message" -d "Commit message used when calling `npm version`." -x
 complete -c vsce -n "__fish_seen_subcommand_from publish" -l "no-git-tag-version" -d "Do not create a version commit and tag when calling `npm version`."
 complete -c vsce -n "__fish_seen_subcommand_from publish" -l "no-update-package-json" -d "Do not update `package.json`."
@@ -62,8 +67,12 @@ complete -c vsce -n "__fish_seen_subcommand_from publish" -l "baseImagesUrl" -d 
 complete -c vsce -n "__fish_seen_subcommand_from publish" -l "yarn" -d "Use yarn instead of npm (default inferred from presence of yarn.lock or .yarnrc)"
 complete -c vsce -n "__fish_seen_subcommand_from publish" -l "no-yarn" -d "Use npm instead of yarn (default inferred from lack of yarn.lock or .yarnrc)"
 complete -c vsce -n "__fish_seen_subcommand_from publish" -l "noVerify" -l "ignoreFile" -d "Indicate alternative .vscodeignore" -r
+complete -c vsce -n "__fish_seen_subcommand_from publish" -l "dependencies" -d "Enable dependency detection via npm or yarn"
 complete -c vsce -n "__fish_seen_subcommand_from publish" -l "no-dependencies" -d "Disable dependency detection via npm or yarn"
 complete -c vsce -n "__fish_seen_subcommand_from publish" -l "pre-release" -d "Mark this package as a pre-release"
+complete -c vsce -n "__fish_seen_subcommand_from publish" -l "allow-star-activation" -d "Allow using * in activation events"
+complete -c vsce -n "__fish_seen_subcommand_from publish" -l "allow-missing-repository" -d "Allow missing a repository URL in package.json"
+complete -c vsce -n "__fish_seen_subcommand_from publish" -l "skip-duplicate" -d "Fail silently if version already exists on the marketplace"
 complete -c vsce -n "__fish_seen_subcommand_from publish" -s "h" -l "help" -d "display help for command"
 
 
@@ -101,4 +110,11 @@ complete -c vsce -n "__fish_seen_subcommand_from show" -s "h" -l "help" -d "disp
 
 
 complete -c vsce -n "__fish_seen_subcommand_from search" -l "json" -d "Output result in json format (default: false)"
+complete -c vsce -n "__fish_seen_subcommand_from search" -l "stats" -d "Shows the extension rating and download counts (default: false)"
+complete -c vsce -n "__fish_seen_subcommand_from search" -s "p" -l "pagesize" -d "Number of results to return (default: \"100\")" -x
 complete -c vsce -n "__fish_seen_subcommand_from search" -s "h" -l "help" -d "display help for command"
+
+
+
+complete -c vsce -n "__fish_seen_subcommand_from help" -s "V" -l "version" -d "output the version number"
+complete -c vsce -n "__fish_seen_subcommand_from help" -s "h" -l "help" -d "display help for command"

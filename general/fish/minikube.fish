9f12b37e8dd71dcd0b86a278f69c7c4e56fe78fa
Yamato Matsuoka <yamaton@gmail.com>
2022-11-17 14:21:28 -0500

    Update minikube

--- general/fish/minikube.fish
+++ general/fish/minikube.fish
@@ -1,6 +1,7 @@
 # Auto-generated with h2o
 
 complete -k -c minikube -n __fish_use_subcommand -x -a completion -d "Generate command completion for a shell"
+complete -k -c minikube -n __fish_use_subcommand -x -a license -d "Outputs the licenses of dependencies to a directory"
 complete -k -c minikube -n __fish_use_subcommand -x -a options -d "Show a list of global command-line options (applies to all commands)."
 complete -k -c minikube -n __fish_use_subcommand -x -a version -d "Print the version of minikube"
 complete -k -c minikube -n __fish_use_subcommand -x -a update-check -d "Print current and latest version number"
@@ -20,9 +21,9 @@ complete -k -c minikube -n __fish_use_subcommand -x -a profile -d "Get or list t
 complete -k -c minikube -n __fish_use_subcommand -x -a config -d "Modify persistent configuration values"
 complete -k -c minikube -n __fish_use_subcommand -x -a addons -d "Enable or disable a minikube addon"
 complete -k -c minikube -n __fish_use_subcommand -x -a image -d "Manage images"
-complete -k -c minikube -n __fish_use_subcommand -x -a cache -d "Add, delete, or push a local image into minikube"
+complete -k -c minikube -n __fish_use_subcommand -x -a cache -d "Manage cache for images"
 complete -k -c minikube -n __fish_use_subcommand -x -a podman-env -d "Configure environment to use minikube's Podman service"
-complete -k -c minikube -n __fish_use_subcommand -x -a docker-env -d "Configure environment to use minikube's Docker daemon"
+complete -k -c minikube -n __fish_use_subcommand -x -a docker-env -d "Provides instructions to point your terminal's docker-cli to the Docker Engine inside minikube. (Useful for building docker images directly inside minikube)"
 complete -k -c minikube -n __fish_use_subcommand -x -a unpause -d "unpause Kubernetes"
 complete -k -c minikube -n __fish_use_subcommand -x -a pause -d "pause Kubernetes"
 complete -k -c minikube -n __fish_use_subcommand -x -a dashboard -d "Access the Kubernetes dashboard running within the minikube cluster"
@@ -33,7 +34,30 @@ complete -k -c minikube -n __fish_use_subcommand -x -a start -d "Starts a local 
 
 
 
-complete -c minikube -n "__fish_seen_subcommand_from start" -l "container-runtime" -d "The container runtime to be used." -r
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "apiserver-name" -d "The authoritative apiserver hostname for apiserver certificates and connectivity." -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "apiserver-port" -d "The apiserver listening port" -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "auto-update-drivers" -d "If set, automatically updates drivers to the latest version." -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "base-image" -d "The base image to use for docker/podman drivers." -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "binary-mirror" -d "Location to fetch kubectl, kubelet, & kubeadm binaries from." -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "cache-images" -d "If true, cache docker images for the current bootstrapper and load them into the machine." -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "cert-expiration" -d "Duration until minikube certificate expiration, defaults to three years (26280h)." -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "cni" -d "CNI plug-in to use." -r
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "container-runtime" -d "The container runtime to be used." -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "cpus" -d "Number of CPUs allocated to Kubernetes." -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "cri-socket" -d "The cri socket path to be used." -r
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "delete-on-failure" -d "If set, delete the current cluster if start fails and try again." -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "disable-driver-mounts" -d "Disables the filesystem mounts provided by the hypervisors" -r
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "disable-metrics" -d "If set, disables metrics reporting (CPU and memory usage), this can improve CPU usage." -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "disable-optimizations" -d "If set, disables optimizations that are set for local Kubernetes." -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "disk-size" -d "Disk size allocated to the minikube VM (format: <number>[<unit>], where unit = b, k, m or g)." -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "dns-domain" -d "The cluster dns domain name used in the Kubernetes cluster" -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "dns-proxy" -d "Enable proxy for NAT DNS requests (virtualbox driver only)" -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "download-only" -d "If true, only download and cache files for later use - don't install or start anything." -r
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "driver" -d "Driver is one of: virtualbox, vmwarefusion, kvm2, qemu2 (experimental), qemu (experimental), vmware, none, docker, podman, ssh (defaults to auto-detect)" -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "dry-run" -d "dry-run mode." -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "embed-certs" -d "if true, will embed the certs in kubeconfig." -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "enable-default-cni" -d "DEPRECATED: Replaced by --cni=bridge" -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "extra-config" -d "A set of key=value pairs that describe configuration that may be passed to different components."
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "extra-disks" -d "Number of extra disks created and attached to the minikube VM (currently only implemented for hyperkit and kvm2 drivers)" -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "feature-gates" -d "A set of key=value pairs that describe feature gates for alpha/experimental features." -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "force" -d "Force minikube to perform possibly dangerous operations" -x
@@ -49,10 +73,9 @@ complete -c minikube -n "__fish_seen_subcommand_from start" -l "image-mirror-cou
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "image-repository" -d "Alternative image repository to pull docker images from." -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "install-addons" -d "If set, install addons." -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "interactive" -d "Allow user prompts for more information" -x
-complete -c minikube -n "__fish_seen_subcommand_from start" -l "iso-url" -d "Locations to fetch the minikube ISO from."
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "iso-url" -d "Locations to fetch the minikube ISO from." -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "keep-context" -d "This will keep the existing kubectl context and will create a minikube context." -x
-complete -c minikube -n "__fish_seen_subcommand_from start" -l "kubernetes-version" -d "The Kubernetes version that the minikube VM will use (ex: v1.2.3, 'stable' for v1.23.3, 'latest' for v1.23.4-rc.0)." -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "kubernetes-version" -d "The Kubernetes version that the minikube VM will use (ex: v1.2.3, 'stable' for v1.25.3, 'latest' for v1.25.3)." -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "kvm-gpu" -d "Enable experimental NVIDIA GPU support in minikube" -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "kvm-hidden" -d "Hide the hypervisor signature from the guest in minikube (kvm2 driver only)" -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "kvm-network" -d "The KVM default network name." -x
@@ -73,18 +96,22 @@ complete -c minikube -n "__fish_seen_subcommand_from start" -l "namespace" -d "T
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "nat-nic-type" -d "NIC Type used for nat network." -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "native-ssh" -d "Use native Golang SSH client (default true)." -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "network" -d "network to run minikube with." -x
-complete -c minikube -n "__fish_seen_subcommand_from start" -l "network-plugin" -d "Kubelet network plug-in to use (default: auto)" -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "network-plugin" -d "DEPRECATED: Replaced by --cni" -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "nfs-shares-root" -d "Where to root the NFS Shares, defaults to /nfsshares (hyperkit driver only)" -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "no-kubernetes" -d "If set, minikube VM/container will start without starting or configuring Kubernetes." -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "no-vtx-check" -d "Disable checking for the availability of hardware virtualization before the vm is started (virtualbox driver only)" -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -s "n" -l "nodes" -d "The number of nodes to spin up." -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -s "o" -l "output" -d "Format to print stdout in." -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "preload" -d "If set, download tarball of preloaded images if available to improve start time." -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "qemu-firmware-path" -d "Path to the qemu firmware file." -r
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "service-cluster-ip-range" -d "The CIDR to be used for service cluster IPs." -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "socket-vmnet-client-path" -d "Path to the socket vmnet client binary" -r
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "socket-vmnet-path" -d "Path to socket vmnet binary" -r
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "ssh-ip-address" -d "IP address (ssh driver only)" -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "ssh-key" -d "SSH key (ssh driver only)" -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "ssh-port" -d "SSH port (ssh driver only)" -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "ssh-user" -d "SSH user (ssh driver only)" -x
+complete -c minikube -n "__fish_seen_subcommand_from start" -l "subnet" -d "Subnet to be used on kic cluster." -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "trace" -d "Send trace events." -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "uuid" -d "Provide VM UUID to restore MAC address (hyperkit driver only)" -x
 complete -c minikube -n "__fish_seen_subcommand_from start" -l "vm" -d "Filter to use only VM Drivers" -x
@@ -94,6 +121,7 @@ complete -c minikube -n "__fish_seen_subcommand_from start" -l "wait-timeout" -d
 
 
 
+complete -c minikube -n "__fish_seen_subcommand_from status" -s "f" -l "format" -d "type: Control Plane host: {{.Host}} kubelet: {{.Kubelet}} apiserver: {{.APIServer}} kubeconfig: {{.Kubeconfig}} {{- if .TimeToStop }} timeToStop: {{.TimeToStop}} {{- end }} {{- if .DockerEnv }} docker-env: {{.DockerEnv}} {{- end }} {{- if .PodManEnv }} podman-env: {{.PodManEnv}} {{- end }} ': Go template format string for the status output." -x
 complete -c minikube -n "__fish_seen_subcommand_from status" -s "l" -l "layout" -d "output layout (EXPERIMENTAL, JSON only): 'nodes' or 'cluster'" -x
 complete -c minikube -n "__fish_seen_subcommand_from status" -s "n" -l "node" -d "The node to check status for." -x
 complete -c minikube -n "__fish_seen_subcommand_from status" -s "o" -l "output" -d "minikube status --output OUTPUT." -x
@@ -110,6 +138,7 @@ complete -c minikube -n "__fish_seen_subcommand_from stop" -l "schedule" -d "Set
 
 
 complete -c minikube -n "__fish_seen_subcommand_from delete" -l "all" -d "Set flag to delete all profiles" -r
+complete -c minikube -n "__fish_seen_subcommand_from delete" -s "o" -l "output" -d "Format to print stdout in." -x
 complete -c minikube -n "__fish_seen_subcommand_from delete" -l "purge" -d "Set this flag to delete the '.minikube' folder from your user directory." -r
 
 
@@ -119,16 +148,11 @@ complete -c minikube -n "__fish_seen_subcommand_from dashboard" -l "url" -d "Dis
 
 
 
-complete -c minikube -n "__fish_seen_subcommand_from pause" -s "A" -l "all-namespaces" -d "If set, pause all namespaces" -x
-complete -c minikube -n "__fish_seen_subcommand_from pause" -s "n" -l "namespaces" -d "namespaces to pause"
-complete -c minikube -n "__fish_seen_subcommand_from pause" -s "n" -l "namespaces" -d "namespaces to pause" -x
 complete -c minikube -n "__fish_seen_subcommand_from pause" -s "o" -l "output" -d "Format to print stdout in." -x
 
 
 
 complete -c minikube -n "__fish_seen_subcommand_from unpause" -s "A" -l "all-namespaces" -d "If set, unpause all namespaces" -x
-complete -c minikube -n "__fish_seen_subcommand_from unpause" -s "n" -l "namespaces" -d "namespaces to unpause"
-complete -c minikube -n "__fish_seen_subcommand_from unpause" -s "n" -l "namespaces" -d "namespaces to unpause" -x
 complete -c minikube -n "__fish_seen_subcommand_from unpause" -s "o" -l "output" -d "Format to print stdout in." -x
 
 
@@ -156,6 +180,7 @@ complete -c minikube -n "__fish_seen_subcommand_from service" -l "wait" -d "Amou
 
 
 
+complete -c minikube -n "__fish_seen_subcommand_from tunnel" -l "bind-address" -d "set tunnel bind address, empty or '*' indicates the tunnel should be available for all interfaces" -x
 complete -c minikube -n "__fish_seen_subcommand_from tunnel" -s "c" -l "cleanup" -d "call with cleanup=true to remove old tunnels" -x
 
 
@@ -193,6 +218,7 @@ complete -c minikube -n "__fish_seen_subcommand_from ip" -s "n" -l "node" -d "Th
 
 
 
+complete -c minikube -n "__fish_seen_subcommand_from logs" -l "audit" -d "Show only the audit logs" -x
 complete -c minikube -n "__fish_seen_subcommand_from logs" -l "file" -d "If present, writes to the provided file instead of stdout." -r
 complete -c minikube -n "__fish_seen_subcommand_from logs" -s "f" -l "follow" -d "Show only the most recent journal entries, and continuously print new entries as they are appended to the journal." -x
 complete -c minikube -n "__fish_seen_subcommand_from logs" -s "n" -l "length" -d "Number of lines back to go within the log" -x
@@ -204,3 +230,7 @@ complete -c minikube -n "__fish_seen_subcommand_from logs" -l "problems" -d "Sho
 complete -c minikube -n "__fish_seen_subcommand_from version" -l "components" -d "list versions of all components included with minikube." -x
 complete -c minikube -n "__fish_seen_subcommand_from version" -s "o" -l "output" -d "One of 'yaml' or 'json'." -x
 complete -c minikube -n "__fish_seen_subcommand_from version" -l "short" -d "Print just the version number." -x
+
+
+
+complete -c minikube -n "__fish_seen_subcommand_from license" -s "d" -l "dir" -d "Directory to output licenses to" -r

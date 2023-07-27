5ba8a424f291d4b79b349fe4fff1132a211bf976
Yamato Matsuoka <yamaton@gmail.com>
2023-07-26 22:25:17 -0400

    Update topgrade and move to general

---
+++ general/fish/topgrade.fish
@@ -0,0 +1,24 @@
+# Auto-generated with h2o
+
+complete -c topgrade -s "c" -l "cleanup" -d "Cleanup temporary or old files"
+complete -c topgrade -l "config" -d "Alternative configuration file" -r
+complete -c topgrade -l "config-reference" -d "Show config reference"
+complete -c topgrade -l "custom-commands" -d "Run only specific custom commands" -x
+complete -c topgrade -l "disable" -d "Do not perform upgrades for the given steps" -x
+complete -c topgrade -l "disable-predefined-git-repos" -d "Don't pull the predefined git repos"
+complete -c topgrade -l "edit-config" -d "Edit the configuration file"
+complete -c topgrade -l "env" -d "Set environment variables" -x
+complete -c topgrade -s "h" -l "help" -d "Print help information"
+complete -c topgrade -s "k" -l "keep" -d "Prompt for a key before exiting"
+complete -c topgrade -l "log-filter" -d "Tracing filter directives." -r
+complete -c topgrade -s "n" -l "dry-run" -d "Print what would be done"
+complete -c topgrade -l "no-retry" -d "Do not ask to retry failed steps"
+complete -c topgrade -l "no-self-update" -d "Don't update Topgrade"
+complete -c topgrade -l "only" -d "Perform only the specified steps (experimental)" -x
+complete -c topgrade -l "remote-host-limit" -d "A regular expression for restricting remote host execution" -x
+complete -c topgrade -l "show-skipped" -d "Show the reason for skipped steps"
+complete -c topgrade -l "skip-notify" -d "Skip sending a notification at the end of a run"
+complete -c topgrade -s "t" -l "tmux" -d "Run inside tmux"
+complete -c topgrade -s "v" -l "verbose" -d "Output debug logs."
+complete -c topgrade -s "V" -l "version" -d "Print version information"
+complete -c topgrade -s "y" -l "yes" -d "Say yes to package manager's prompt" -x
